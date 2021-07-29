58c3e4d66492b559995c4ad25302baf6a8c587e0
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-07-29 13:59:20 +0900

    create

---
+++ java1/Programming/src/ClassApp.java
@@ -0,0 +1,10 @@
+public class ClassApp {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(2.1));
+		System.out.println(Math.ceil(2.1));
+
+	}
+
+}
