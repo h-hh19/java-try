24b17bf4b7da750c77e40e7757f20276e0095178
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-07-30 16:24:21 +0900

    javaMethod

---
+++ java1/ProjectMethod/src/FirstMethod.java
@@ -0,0 +1,10 @@
+
+public class FirstMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello Method!");
+			System.out.println(Math.floor(1.74));
+	}
+
+}
---
+++ java1/ProjectMethod/src/whyMethod.java
@@ -0,0 +1,14 @@
+public class whyMethod {
+	
+	public static void printTowTimesM() {
+		System.out.println("M");
+		System.out.println("M");
+		System.out.println("M");
+		
+	}
+	
+	public static void main(String[] args) {
+		printTowTimesM();
+	}
+
+}
