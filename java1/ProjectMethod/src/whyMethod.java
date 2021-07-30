c5b262efd0fa9df7076c69bef1fb62c570dc45ec
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-07-30 16:50:34 +0900

    method try

--- java1/ProjectMethod/src/whyMethod.java
+++ java1/ProjectMethod/src/whyMethod.java
@@ -1,14 +1,16 @@
 public class whyMethod {
 	
-	public static void printTowTimesM() {
-		System.out.println("M");
-		System.out.println("M");
-		System.out.println("M");
+	public static void printTowTimes(String text, String delimiter) {
+		System.out.println(delimiter);
+
+		System.out.println(text);
+		System.out.println(text);
+		System.out.println(text);
 		
 	}
 	
 	public static void main(String[] args) {
-		printTowTimesM();
+		printTowTimes("m","377");
 	}
 
 }
