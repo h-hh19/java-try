d7ed8964b36a0006bebb2d618ae75e6476d54f96
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-08-02 09:56:34 +0900

    java practice

--- java1/ProjectMethod/src/whyMethod.java
+++ java1/ProjectMethod/src/whyMethod.java
@@ -1,16 +1,29 @@
 public class whyMethod {
 	
-	public static void printTowTimes(String text, String delimiter) {
+	public static void printTwoTimes(String text, String delimiter) {
 		System.out.println(delimiter);
 
 		System.out.println(text);
 		System.out.println(text);
 		System.out.println(text);
 		
+		//-----------------------------
+		
+		
+	}
+	
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out = out + delimiter + "\n";
+		out = out + text + "\n";
+		return out;
 	}
 	
 	public static void main(String[] args) {
-		printTowTimes("m","377");
+		
+		printTwoTimes("m","377");
+
+		System.out.println(twoTimes("17","3"));
 	}
 
 }
