e61d284a1b3f1f7f1e2993fef5cb250b02cecfaf
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-08-03 17:39:19 +0900

    java try

---
+++ java1/BaekJoonTRY/src/Math10430.java
@@ -0,0 +1,16 @@
+
+public class Math10430 {
+
+	public static void main(String[] args) {
+		
+		int Y = 2021;
+	      if((Y%4==0) && (Y%100!=0||Y%400==0)){
+	            System.out.println("1");
+	        }else{
+	            System.out.println("0");
+	        }
+	    }
+
+	}
+
+
